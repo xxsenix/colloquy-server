84a5c198481063a7f5b5948d61ccdcae0db703ec
Travis Callahan <callahantravis29@gmail.com>
2019-06-06 22:39:32 -0500

    edited CORS

--- server.js
+++ server.js
@@ -20,21 +20,24 @@ const app = express();
 app.use(morgan("common"));
 
 // CORS
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  if (req.method === "OPTIONS") {
-    return res.send(204);
-  }
-  next();
-});
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   if (req.method === "OPTIONS") {
+//     return res.send(204);
+//   }
+//   next();
+// });
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
