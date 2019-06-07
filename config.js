71ca2e0f4f9baf503bc6ec0602062d08389f6e19
Travis Callahan <callahantravis29@gmail.com>
2019-06-06 22:29:59 -0500

    Added CORS.

--- config.js
+++ config.js
@@ -6,3 +6,4 @@ exports.TEST_DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || "secret";
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const passport = require('passport');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+const cors = require("cors");
 
-const { router: usersRouter } = require('./users');
-const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
-const postsRouter = require('./posts/postsRouter');
+const { router: usersRouter } = require("./users");
+const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
+const postsRouter = require("./posts/postsRouter");
 
 mongoose.Promise = global.Promise;
 
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require("./config");
 
 const app = express();
 
 // Logging
-app.use(morgan('common'));
+app.use(morgan("common"));
 
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
     return res.send(204);
   }
   next();
@@ -32,37 +39,37 @@ app.use(function (req, res, next) {
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-app.use('/api/posts/', postsRouter);
-app.use('/api/users/', usersRouter);
-app.use('/api/auth/', authRouter);
+app.use("/api/posts/", postsRouter);
+app.use("/api/users/", usersRouter);
+app.use("/api/auth/", authRouter);
 
-const jwtAuth = passport.authenticate('jwt', { session: false });
+const jwtAuth = passport.authenticate("jwt", { session: false });
 
 // A protected endpoint which needs a valid JWT to access it
-app.get('/api/protected', jwtAuth, (req, res) => {
+app.get("/api/protected", jwtAuth, (req, res) => {
   return res.json({
-    data: 'rosebud'
+    data: "rosebud"
   });
 });
 
-app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'Not Found' });
+app.use("*", (req, res) => {
+  return res.status(404).json({ message: "Not Found" });
 });
 
 let server;
 
 function runServer(databaseUrl, port = PORT) {
-
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on("error", err => {
           mongoose.disconnect();
           reject(err);
         });
@@ -73,7 +80,7 @@ function runServer(databaseUrl, port = PORT) {
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('Closing server');
+      console.log("Closing server");
       server.close(err => {
         if (err) {
           return reject(err);
